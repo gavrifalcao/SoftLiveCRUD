143dd7864fbc3b4eb924a916c82da9a8def0d8c2
gavrifalcao <gavrifalcao@gmail.com>
2025-07-13 21:05:59 -0400

    fix: adaptação do filtro de categoria em filtroService.ts por limitação da MockAPI

--- src/services/filtroService.ts
+++ src/services/filtroService.ts
@@ -1,14 +1,23 @@
 import api from './api';
 import type { Product } from '../types/Product';
+import { getProducts } from './productService';
+
 
 export const fetchByName = async (nome: string): Promise<Product[]> => {
   const res = await api.get<Product[]>(`recursos?nome=${encodeURIComponent(nome)}`);
   return res.data;
 };
 
+/* Função apropriada a se usar para buscar produtos por categoria */
+// export const fetchByCategory = async (categoria: string): Promise<Product[]> => {
+//   const res = await api.get<Product[]>(`recursos?categoria=${encodeURIComponent(categoria)}`);
+//   return res.data;
+// };
+
+// Função adaptada para buscar produtos por categoria por limitações da MockAPI
 export const fetchByCategory = async (categoria: string): Promise<Product[]> => {
-  const res = await api.get<Product[]>(`recursos?categoria=${encodeURIComponent(categoria)}`);
-  return res.data;
+  const res = await getProducts();
+  return res.data.filter(p => p.categoria.toLowerCase() === categoria.toLowerCase());
 };
 
 export const filterByPrice = (products: Product[], max: number): Product[] => {
